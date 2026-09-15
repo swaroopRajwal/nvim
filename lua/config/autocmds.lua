cf90810171ccfe447d63f8e866a877e8a95aca1a
swaroopRajwal <swaroop.rajwal18@gmail.com>
2026-09-15 10:35:34 +0530

    auto reload files on window focus to show external changes as well

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Trigger file check when the window regains focus
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = vim.api.nvim_create_augroup("auto_reload_files", { clear = true }),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
