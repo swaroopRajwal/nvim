b043286fdb822bd93e798cb652a261bbd237fb9c
swaroopRajwal <swaroop.rajwal18@gmail.com>
2026-09-15 10:36:11 +0530

    enabled line wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = vim.fn.expand("~/.neovim-venv/bin/python")
+vim.opt.wrap = true
